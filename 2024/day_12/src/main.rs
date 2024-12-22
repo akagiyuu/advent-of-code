41bf7ffb5c564dfc1752b5fd3ce539c604cd9619
akagiyuu <khanghuynh0245@gmail.com>
2024-12-22 12:36:42 +0700

    feat(2024): complete day 12

--- 2024/day_12/src/main.rs
+++ 2024/day_12/src/main.rs
@@ -1,142 +1,103 @@
-use std::collections::{BTreeSet, HashSet};
+use itertools::Itertools;
 
 const INPUT: &str = include_str!("../input.txt");
 
 const MAIN_DIRECTION: [(isize, isize); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
 
-fn calculate_total_price(garden: &[Vec<u8>]) -> usize {
-    let row_count = garden.len() as isize;
-    let column_count = garden[0].len() as isize;
+fn into_valid((x, y): (isize, isize), group_c: u8, garden: &[Vec<u8>]) -> Option<(usize, usize)> {
+    let n = garden.len() as isize;
+    let m = garden[0].len() as isize;
 
-    let mut total_price = 0;
-    let mut is_visited = vec![vec![false; column_count as usize]; row_count as usize];
-    let mut stack = BTreeSet::new();
-
-    for (i, row) in garden.iter().enumerate() {
-        for (j, &group) in row.iter().enumerate() {
-            if is_visited[i][j] {
-                continue;
-            };
-            let mut perimeter = 0;
-            let mut area = 0;
-
-            stack.clear();
-            stack.insert((i, j));
-            while let Some((i, j)) = stack.pop_first() {
-                is_visited[i][j] = true;
-                area += 1;
-                perimeter += 4;
-
-                for (di, dj) in MAIN_DIRECTION {
-                    let next_i = i as isize + di;
-                    let next_j = j as isize + dj;
-                    if !(0..row_count).contains(&next_i) || !(0..column_count).contains(&next_j) {
-                        continue;
-                    }
-                    let [next_i, next_j] = [next_i, next_j].map(|x| x as usize);
-                    if garden[next_i][next_j] == group {
-                        perimeter -= 1;
-
-                        if !is_visited[next_i][next_j] {
-                            stack.insert((next_i, next_j));
-                        }
-                    }
-                }
-            }
-
-            total_price += perimeter * area;
-        }
+    if !(0..n).contains(&x) || !(0..m).contains(&y) {
+        return None;
     }
 
-    total_price
+    if garden[x as usize][y as usize] == group_c {
+        Some((x as usize, y as usize))
+    } else {
+        None
+    }
 }
 
-fn calculate_total_price_with_discount(garden: &[Vec<u8>]) -> u64 {
-    let row_count = garden.len() as isize;
-    let column_count = garden[0].len() as isize;
+fn perimeter((x, y): (usize, usize), group_c: u8, garden: &[Vec<u8>]) -> usize {
+    MAIN_DIRECTION
+        .into_iter()
+        .filter(|(dx, dy)| {
+            into_valid((x as isize + dx, y as isize + dy), group_c, garden).is_none()
+        })
+        .count()
+}
 
-    let mut total_price = 0;
-    let mut is_visited = vec![vec![false; column_count as usize]; row_count as usize];
-    let mut stack = BTreeSet::new();
-    let mut vertices = HashSet::new();
+fn corner((x, y): (usize, usize), group_c: u8, garden: &[Vec<u8>]) -> usize {
+    let [nw, w, sw, n, s, ne, e, se]: [bool; 8] = (-1..2)
+        .cartesian_product(-1..2)
+        .filter(|(dx, dy)| dx | dy != 0)
+        .map(|(dx, dy)| into_valid((x as isize + dx, y as isize + dy), group_c, garden).is_some())
+        .collect_vec()
+        .try_into()
+        .unwrap();
+    [
+        n && w && !nw,
+        n && e && !ne,
+        s && w && !sw,
+        s && e && !se,
+        !(n || w),
+        !(n || e),
+        !(s || w),
+        !(s || e),
+    ]
+    .into_iter()
+    .filter(|&x| x)
+    .count()
+}
 
-    for (i, row) in garden.iter().enumerate() {
-        for (j, &group) in row.iter().enumerate() {
-            if is_visited[i][j] {
+fn calculate_total_price(
+    garden: &[Vec<u8>],
+    cell_value: impl Fn((usize, usize), u8, &[Vec<u8>]) -> usize,
+) -> usize {
+    let n = garden.len();
+    let m = garden[0].len();
+
+    let mut price = 0;
+
+    let mut is_visited = vec![vec![false; m]; n];
+    let mut stack = vec![];
+
+    for (cx, row) in garden.iter().enumerate() {
+        for (cy, &group_c) in row.iter().enumerate() {
+            if is_visited[cx][cy] {
                 continue;
-            };
-            eprintln!("DEBUGPRINT[10]: main.rs:64: group={:#?}", group as char);
-            let mut area = 0;
+            }
 
-            vertices.clear();
+            let mut group_area = 0;
+            let mut group_value = 0;
             stack.clear();
-            stack.insert((i, j));
-            while let Some((i, j)) = stack.pop_first() {
-                is_visited[i][j] = true;
-                area += 1;
+            stack.push((cx, cy));
 
-                for dvi in [0, 1] {
-                    for dvj in [0, 1] {
-                        vertices.insert((i + dvi, j + dvj));
-                    }
+            while let Some((x, y)) = stack.pop() {
+                if is_visited[x][y] {
+                    continue;
                 }
-                for (di, dj) in MAIN_DIRECTION {
-                    let next_i = i as isize + di;
-                    let next_j = j as isize + dj;
-                    if !(0..row_count).contains(&next_i) || !(0..column_count).contains(&next_j) {
-                        continue;
-                    }
-                    let [next_i, next_j] = [next_i, next_j].map(|x| x as usize);
-                    if garden[next_i][next_j] == group && !is_visited[next_i][next_j] {
-                        stack.insert((next_i, next_j));
-                    }
-                }
+                is_visited[x][y] = true;
+
+                group_area += 1;
+                group_value += cell_value((x, y), group_c, garden);
+
+                let neighbors = MAIN_DIRECTION.into_iter().filter_map(|(dx, dy)| {
+                    into_valid((x as isize + dx, y as isize + dy), group_c, garden)
+                });
+                stack.extend(neighbors);
             }
-
-            let mut corner_count = 0;
-            for &(i, j) in &vertices {
-                let mut neighbor = 0;
-
-                for di in [-1, 0, 1] {
-                    for dj in [-1, 0, 1] {
-                        if di == 0 && dj == 0 {
-                            continue;
-                        }
-
-                        let next_i = i as isize + di;
-                        let next_j = j as isize + dj;
-                        if !(0..=row_count).contains(&next_i)
-                            || !(0..=column_count).contains(&next_j)
-                        {
-                            continue;
-                        }
-                        let [next_i, next_j] = [next_i, next_j].map(|x| x as usize);
-                        if vertices.contains(&(next_i, next_j)) {
-                            if i == 1 && j == 1 {
-                                println!("{}, {}", next_i, next_j);
-                            }
-                            neighbor += 1;
-                        }
-                    }
-                }
-                //
-                // println!("{}, {} -> {}", i, j, neighbor);
-                match neighbor {
-                    3 | 4 | 7 => corner_count += 1,
-                    _ => {}
-                }
-            }
-
-            total_price += corner_count * area;
+            price += group_area * group_value;
         }
     }
 
-    total_price
+    price
 }
 
 fn main() {
     let garden: Vec<_> = INPUT.lines().map(|line| line.as_bytes().to_vec()).collect();
 
-    println!("{}", calculate_total_price(&garden));
-    println!("{}", calculate_total_price_with_discount(&garden));
+    println!("{}", calculate_total_price(&garden, perimeter));
+    println!("{}", calculate_total_price(&garden, corner));
 }
