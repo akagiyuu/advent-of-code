e301868149afaab8e31f54b591e5e980264ccfcf
akagiyuu <khanghuynh0245@gmail.com>
2025-12-17 14:28:03 +0700

    feat(2025): complete day 1

---
+++ 2025/day-1/part-1.rs
@@ -0,0 +1,48 @@
+use std::io::stdin;
+
+const MOD: u64 = 100;
+
+const fn next(a: u64) -> u64 {
+    if a == MOD - 1 {
+        0
+    } else {
+        a + 1
+    }
+}
+
+const fn prev(a: u64) -> u64 {
+    if a == 0 {
+        MOD - 1
+    } else {
+        a - 1
+    }
+}
+
+fn main() {
+    let mut cur = 50;
+    let mut res = 0;
+
+    while let Some(Ok(line)) = stdin().lines().next() {
+        let direction = line.as_bytes()[0];
+        let cnt: u64 = line.as_str()[1..].parse().unwrap();
+
+        match direction {
+            b'L' => {
+                for _ in 0..cnt {
+                    cur = next(cur);
+                }
+            }
+            b'R' => {
+                for _ in 0..cnt {
+                    cur = prev(cur);
+                }
+            }
+            _ => unreachable!(),
+        }
+        if cur == 0 {
+            res += 1;
+        }
+    }
+
+    println!("{res}");
+}
